e0566ff2a9c4715e25982386ba60f90de4615e83
Brian Wylie <bwylie@clicksecurity.com>
2013-12-02 19:15:02 -0700

    not sure what changed; looks just like white space

--- data_hacking/simple_stats/simple_stats.py
+++ data_hacking/simple_stats/simple_stats.py
@@ -28,25 +28,25 @@ class FixedHeap(list):
         self._N = N
         super(list, self).__init__()
     def push(self, item):
-        if (len(self) == self._N): 
+        if (len(self) == self._N):
             heapq.heappushpop(self,item)
-        else: 
+        else:
             heapq.heappush(self, item)
     def sorted(self):
         return heapq.nlargest(self._N, self)
     def max(self):
         return heapq.nlargest(1, self)[0]
-    
+
 class GTest():
-    ''' This is homegrown functionality for contingency table, and G-test 
+    ''' This is homegrown functionality for contingency table, and G-test
         for two pandas series with categorical (nominal) values. G-test is
         for goodness of fit to a distribution and for independence in contingency
         tables. It's related to chi-squared, multinomial and Fisher's exact test,
         please see http://en.wikipedia.org/wiki/G_test (Wikipedia).
         Disclaimer: We have gone off the reservation here. This code isn't generalized
-                    and isn't well tested, there may be bugs (will be bugs), 
+                    and isn't well tested, there may be bugs (will be bugs),
                     please use another method if available.
-       
+
     '''
     def __init__(self):
         ''' Init for GTest '''
@@ -68,22 +68,22 @@ class GTest():
                     3) df: A dataframe with keys from series_a as the index, series_b_keys as the
                        columns, and the counts as the values.
 
-                    Note: Just including the max g-score in the output right now as the g-score 
+                    Note: Just including the max g-score in the output right now as the g-score
                     without context is kinda meaningless.
         '''
 
-        # Might Need Improvement: 
-        #     Expected counts, assuming uniform distribution under the null hypothesis. 
-        #     Which in this case means that series_a's categories will be equally 
+        # Might Need Improvement:
+        #     Expected counts, assuming uniform distribution under the null hypothesis.
+        #     Which in this case means that series_a's categories will be equally
         #     distributed among series_b's categories.
-        #     
+        #
         #     Note: I might need some private lessons from Alison Gibbs on exactly how
         #           this should be done, she is SO cute! (and also super smart of course :)
         #           http://www.youtube.com/watch?v=0nmxFpNBFIY
         total_counts_a = series_a.value_counts()
         num_categories_b = series_b.unique().size
         expected_count_a = total_counts_a / float(num_categories_b)
-    
+
         # Count up all the times that a category from series_a
         # matches up with a category from series_b. This is
         # basically a gigantic contingency table
@@ -96,7 +96,7 @@ class GTest():
 
         # Now that we have a contingency table we can compare
         # the counts against the expected counts given our
-        # null hypothesis that each category should have a 
+        # null hypothesis that each category should have a
         # uniform distribution across all other categories.
         a_b_scores = []
 
@@ -104,12 +104,12 @@ class GTest():
         for key_a, counter in _cont_table.iteritems():
             score_heap = FixedHeap(matches)
             for key_b, count in counter.iteritems():
-                score = self.g_test_score(count, expected_count_a[key_a])
+                score = self._g_test_score(count, expected_count_a[key_a])
                 score_heap.push((score, count, key_b))
 
             # We want to convert the sorted list into an ordered dictionary (for dataframe later)
             od = collections.OrderedDict([(item[2], item[1]) for item in score_heap.sorted()])
-            
+
             # Now add the ordered dict to the meta list
             a_b_scores.append({'key':key_a, 'max_g': score_heap.max()[0], 'matches': od})
 
@@ -128,7 +128,7 @@ class GTest():
         df = pd.DataFrame(match_list, index=a_keys)
         return a_keys, match_list, df
 
-    def g_test_score(self, count, expected):
+    def _g_test_score(self, count, expected):
         #return count/expected
         ''' G Test Score for likelihood ratio stats '''
         if (count == 0):
@@ -139,7 +139,7 @@ class GTest():
 
 # Simple test of the functionality
 def _test():
-    
+
     import os
     import pprint
 
@@ -152,15 +152,15 @@ def _test():
     #    date,domain,ip,reverse,description,registrant,asn,inactive,country
     dataframe = pd.read_csv(file_path, names=['date','domain','ip','reverse','description',
                                              'registrant','asn','inactive','country'], header=None)
-    
-    # For this use case we're going to remove any rows that have a '-' in the data 
+
+    # For this use case we're going to remove any rows that have a '-' in the data
     # by replacing '-' with NaN and then running dropna() again
     dataframe = dataframe.replace('-', np.nan)
     dataframe = dataframe.dropna()
 
     # We noticed that many values just differed by captilization (this is common)
     dataframe = dataframe.applymap(lambda x: x.strip().lower() if isinstance(x,str) else x)
-    
+
     # Exploits with highest correlation to ASNs
     g_test = GTest()
     exploits, match_list, df = g_test.highest_gtest_scores(dataframe['description'], dataframe['asn'], N=5, matches=10)
@@ -169,7 +169,7 @@ def _test():
 
     exploits, match_list, df = g_test.highest_gtest_scores(dataframe['description'], dataframe['asn'], N=10, matches=10, reverse=True, min_volume=500)
     print '\n<<< Exploits with lowest correlation to ASNs >>>'
-    pprint.pprint(zip(exploits, match_list))   
+    pprint.pprint(zip(exploits, match_list))
 
 if __name__ == "__main__":
     _test()
