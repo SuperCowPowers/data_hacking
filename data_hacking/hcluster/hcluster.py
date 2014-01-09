4ef249cc8a521b47929c9482f9b7cebe3f7af2ec
Brian Wylie <bwylie@clicksecurity.com>
2014-01-09 10:07:01 -0700

    removing the ability for user to specify labels (for now) too many issues with it. Added better support for aggregation of nodes based on close similarity. Also now returning the H graph and root node

--- data_hacking/hcluster/hcluster.py
+++ data_hacking/hcluster/hcluster.py
@@ -1,4 +1,4 @@
-''' 
+'''
     Use single-linkage hierarchical clustering to build up a tree
     of clusters from a similarity list.
         - Similarities -> Hierarchical Clustering (Tree)
@@ -19,7 +19,7 @@ import pandas as pd
 import data_hacking.lsh_sims as lsh_sims
 
 class HCluster():
-    ''' 
+    '''
         Use single-linkage hierarchical clustering to build up a tree
         of clusters from a similarity list.
             - Similarities -> Hierarchical Clustering (Tree)
@@ -57,40 +57,39 @@ class HCluster():
         self.agg_sim = 0.0
 
     def set_sim_method(self, sim_function):
-        ''' 
+        '''
             The similarity function needs to take two args (features1, features2)
             and return the similarity of those features as a float between 0-1
         '''
         self.sim_method = sim_function
 
-    def sims_to_hcluster(self, sim_list, labels=None, agg_sim=0.0):
-        ''' 
+    def sims_to_hcluster(self, sim_list,  agg_sim=0.0):
+        '''
             Use single-linkage hierarchical clustering to build up a tree
             of clusters from a similarity list.
                 - Similarities -> Hierarchical Clustering (Tree)
 
-            Input: sim_list - a tuple (source, target, sim)
+            Input: sim_list - a tuple (sim, source, target)
 
-            Output: Hierarchical Cluster (as a networkx digraph datatype)
+            Output: Hierarchical Cluster (as a networkx digraph datatype), Graph_Root
         '''
         self.agg_sim = agg_sim
 
-        # Generate labels if we need to
-        if labels is None:
-            labels = []
-            if self._record_type == 'list':
-                for record in self._records:
-                    labels.append(':'.join(record))
-            else:
-                for uuid in xrange(self._records.shape[0]):
-                    labels.append(':'.join(self._get_row(uuid)))
+        # Generate labels
+        labels = []
+        if self._record_type == 'list':
+            for record in self._records:
+                labels.append(':'.join(record))
+        else:
+            for uuid in xrange(self._records.shape[0]):
+                labels.append(':'.join(self._get_row(uuid)))
 
 
         # Sort the sim_list based on similarity, source, target
         # this triple key sort insures that the hierarchical tree
         # is constructed correctly, if this doesn't happen the
         # tree will be incorrect!
-        sim_list.sort(key=lambda k:k, reverse=True)
+        sim_list.sort(key=lambda k: k, reverse=True)
 
         # Now construct a aggregation tree
         meta_nodes = collections.defaultdict(set)
@@ -101,7 +100,7 @@ class HCluster():
         exemplars = []
         components = nx.weakly_connected_component_subgraphs(graph)
         for comp in components:
-            
+
             # Find the root of this component
             root = self.find_root(graph, comp.nodes()[0])
 
@@ -117,7 +116,6 @@ class HCluster():
         exemplar_sims = []
         exemplar_zero_sims = []
         joined_exemplars = set()
-        all_exemplars = set([x[0] for x in exemplars])
         print "Created exemplar set (%d)" % (len(exemplars))
         for node1, features1 in exemplars:
             for node2, features2 in exemplars:
@@ -152,46 +150,43 @@ class HCluster():
             graph.node[meta_node]['label'] = meta_label
 
         # Logic around Root node
-        if (not exemplar_zero_sims):
-            root = self.find_root(graph, graph.nodes()[0])
-            graph.add_node(root, label='Root')
-        else:
+        if (exemplar_zero_sims):
             graph.add_node('Root', label='Root')
             #graph.add_edge('Root', last_meta_node, weight=0, inv_weight=1.0)
             for sim, source, target in exemplar_zero_sims:
                 graph.add_edge('Root', source, weight=sim, inv_weight=1.0-sim)
                 graph.add_edge('Root', target, weight=sim, inv_weight=1.0-sim)
 
-        #graph = nx.weakly_connected_component_subgraphs(graph)[0]
+        # Sanity check
+        if (graph.number_of_nodes() == 0):
+            print '<<<< WTF Error: Looks like an empty graph >>>>>'
+            print 'Graph %d nodes %d edges' % (graph.number_of_nodes(), graph.number_of_edges())
+            return None, None
 
-        # Now compute 'maximal' spanning tree
-        print "Computing Maximal Spanning Tree..."
-        #tree = nx.minimum_spanning_tree(graph, weight='weight')
-        #tree = nx.minimum_spanning_tree(graph.to_undirected(), weight='inv_weight')
-
-        # Return the both the graph and the minimal spanning tree
-        return graph, graph
+        # Return both the graph and the root
+        root_node = self.find_root(graph, graph.nodes()[0])
+        return graph, root_node
 
     def add_meta_node(self, G, sim, source, target):
 
         # Find the roots of both source and target
         root_source = self.find_root(G, source)
-        root_target = self.find_root(G, target)        
+        root_target = self.find_root(G, target)
 
         # Add the meta node
         meta_node = str(source)+'_'+str(target)
-        G.add_node(meta_node)            
+        G.add_node(meta_node)
         G.add_edge(meta_node, root_source, weight=sim, inv_weight=1.0-sim)
         G.add_edge(meta_node, root_target, weight=sim, inv_weight=1.0-sim)
-    
+
         # Add the meta label
         meta_label = self.meta_label(G, meta_node)
-        G.node[meta_node]['label'] = meta_label 
+        G.node[meta_node]['label'] = meta_label
 
 
     def check_edge_map(self, edge_map, G, node, sim):
         for target, t_sim in edge_map[node]:
-            if (target in G and t_sim == sim):
+            if (target in G and self.close_sim(t_sim, sim)):
                 return target
         return None
 
@@ -212,59 +207,52 @@ class HCluster():
             # Three cases
             # 1) Neither node is in the graph
             # 2) One node is in the graph
-            #     a) the sim is the same
+            #     a) the sim is the same/close
             #     b) the sim is different
             # 3) They are both in the graph
-            
+
             # 1) Neither node is in the graph
             if (source not in graph and target not in graph):
 
                 # Check edge map first before we go and add two new nodes and a meta node
                 new_target = self.check_edge_map(edge_map, graph, source, sim)
-                if (new_target):
-                    root_target = self.find_root(graph, new_target)
-                    graph.add_node(source, label=labels[source])
-                    graph.add_edge(root_target, source, weight=sim, inv_weight=1.0-sim)
-                    continue
                 new_source = self.check_edge_map(edge_map, graph, target, sim)
-                if (new_source):
-                    root_source = self.find_root(graph, new_source)
-                    graph.add_node(target, label=labels[source])
-                    graph.add_edge(root_source, target, weight=sim, inv_weight=1.0-sim)
-                    continue
+                if (new_target):
+                    target = new_target
+                elif (new_source):
+                    source = new_source
 
                 # Okay nothing hit so we have to add everything
-                graph.add_node(source, label=labels[source])
-                graph.add_node(target, label=labels[target])
+                else:
+                    graph.add_node(source, label=labels[source])
+                    graph.add_node(target, label=labels[target])
 
-                # Add the meta node
-                self.add_meta_node(graph, sim, source, target)
-                continue
+                    # Add the meta node
+                    self.add_meta_node(graph, sim, source, target)
+                    continue
 
-            # 2) One node is in the graph (source)
+            # 2) One node is in the graph (target)
             if (source not in graph):
                 graph.add_node(source, label=labels[source])
-                root_target = self.find_root(graph, target)
-                
-                # 2a) similarity is the same (source)
-                if self.close_sim(sim, graph.out_edges(root_target, data=True)[0][2]['weight']):
-                # if (sim == graph.out_edges(root_target, data=True)[0][2]['weight']):
-                    graph.add_edge(root_target, source, weight=sim, inv_weight=1.0-sim)
+                target_parent = self.find_parent(graph, target)
+
+                # 2a) similarity is the same (or close)
+                if self.close_sim(sim, graph.out_edges(target_parent, data=True)[0][2]['weight']):
+                    graph.add_edge(target_parent, source, weight=sim, inv_weight=1.0-sim)
                     continue
                 # 2b) similarity is NOT the same
                 else:
                     self.add_meta_node(graph, sim, source, target)
-    
-            # 2) One node is in the graph (target)
+
+            # 2) One node is in the graph (source)
             elif (target not in graph):
                 graph.add_node(target, label=labels[target])
-                root_source = self.find_root(graph, source)
-                
-                # 2a) similarity is the same (target)
-                if self.close_sim(sim, graph.out_edges(root_source, data=True)[0][2]['weight']):
-                #if (sim == graph.out_edges(root_source, data=True)[0][2]['weight']):
-                    graph.add_edge(root_source, target, weight=sim, inv_weight=1.0-sim)
-                    continue 
+                source_parent = self.find_parent(graph, source)
+
+                # 2a) similarity is the same (or close)
+                if self.close_sim(sim, graph.out_edges(source_parent, data=True)[0][2]['weight']):
+                    graph.add_edge(source_parent, target, weight=sim, inv_weight=1.0-sim)
+                    continue
                 # 2b) similarity is NOT the same
                 else:
                     self.add_meta_node(graph, sim, target, source)
@@ -289,18 +277,21 @@ class HCluster():
         while (G.predecessors(root)):
             root = G.predecessors(root)[0]
         return root
-    
+
+    def find_parent(self, G, node):
+        return G.predecessors(node)[0]
+
     def common_features(self, G, root):
-        features = None
-        for node in G.successors(root):
-            if features:
-                features = features.intersection(set(G.node[node]['label'].split(':')))
-            else:
-                features = set(G.node[node]['label'].split(':'))
-        return ':'.join(features)
+        successors = G.successors(root)
+        if not successors: return '---'
+        common = G.node[successors[0]]['label'].split(':')
+        for node in successors[1:]:
+            node_feature_set = set(G.node[node]['label'].split(':'))
+            common = [f for f in common if f in node_feature_set]
+        return ':'.join(common)
 
     def exemplar_features(self, G, root):
-        return set(G.node[root]['label'].split(':'))
+        return G.node[root]['label'].split(':')
 
     def is_meta_node(self, node_id):
         return not isinstance(node_id, int)
@@ -325,7 +316,6 @@ class HCluster():
 
         # Find the root
         root_node = [n for n,d in h_tree.in_degree().items() if d==0][0]
-        print "Root node = %s" % (root_node)
 
         # Tree layout
         pos = nx.graphviz_layout(h_tree, root=root_node, prog=prog)
@@ -335,11 +325,11 @@ class HCluster():
         # Specifiy edge labels explicitly
         edge_labels=dict([((u,v,),str(d['weight'])[:4]) for u,v,d in h_tree.edges(data=True)])
         nx.draw_networkx_edge_labels(h_tree,pos,edge_labels=edge_labels)
-        plt.show()          
+        plt.show()
 
 # Simple test of the hcluster functionality
 def _test():
-    
+
     import pprint
 
     # Construct a small dataset
@@ -351,7 +341,7 @@ def _test():
                ['x','y','z'],
                ['w','x','q','z','y'],
                ['r','s','t'],
-               ['u','s','t']  
+               ['u','s','t']
                ]
 
     # Note: The parameters here are setup for feeding the results into a Hierarchical
@@ -359,25 +349,25 @@ def _test():
     params = {'num_hashes':20, 'lsh_bands':20, 'lsh_rows':1, 'drop_duplicates':True}
     lsh = lsh_sims.LSHSimilarities(my_data, mh_params=params)
     sims = lsh.batch_compute_similarities(distance_metric='jaccard', threshold=.01)
-    
+
     print 'All similarity pairs'
     pprint.pprint(sims)
-    
+
     print 'Query on [x,y,z,h]'
     matches = lsh.similarity_query(['x','y','z','h'])
     pprint.pprint(matches)
-    
+
     print 'Top 5 on [x,y,z,h]'
     top_5 = lsh.top_N(['x','y','z','h'], my_data, 5)
     pprint.pprint(top_5)
 
     # Compute a hierarchical clustering from the similarity list
-    h_clustering = HCluster()
+    h_clustering = HCluster(my_data)
     h_clustering.set_sim_method(lsh.jaccard_sim)
-    h_graph, h_tree = h_clustering.sims_to_hcluster(sims, my_data)
+    h_graph, root = h_clustering.sims_to_hcluster(sims)
 
     # Plot the hierarchical tree
-    h_clustering.plot_htree(h_tree)
+    h_clustering.plot_htree(h_graph)
 
 
 if __name__ == "__main__":
